c0a79ae2e395a08284c2ac1ca06507c0930f7053
Márcio Sena <contatomarciosena@gmail.com>
2023-07-31 12:02:17 -0300

    aded

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,7 @@ import { AddArea } from './components/AddArea';
 
 const App = () => {
   const [list, setList] = useState<Item[]>([
-    { id: 1, name: 'Comprar o pão na padaria', done: false },
-    { id: 2, name: 'Comprar um bolo na padaria', done: true },
+ 
   ]);
 
   const handleAddTask = (taskName: string) => {
